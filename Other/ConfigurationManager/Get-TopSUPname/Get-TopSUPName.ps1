ce4ca706ccb683acc0e0eeeb02db1d7962c22f56
LeeViewB <31350302+LeeViewB@users.noreply.github.com>
2022-12-07 10:18:18 +0200

    Added Get-TopSUPname PS script

---
+++ Other/ConfigurationManager/Get-TopSUPname/Get-TopSUPName.ps1
@@ -0,0 +1,54 @@
+﻿Function Set-CMDrive {
+    <#
+    .SYNOPSIS
+    Import ConfigMgr module, create ConfigrMgr PS drive and set location to it.
+    .DESCRIPTION
+    Set current working directory to site code for access to ConfigMgr cmdlets. Some validation is in place to verify the site code marrys up to be of $Server.
+    Called by main body.
+    #>
+    Param(
+        [string]$SiteCode,
+        [string]$Server,
+        [string]$Path
+    )
+
+    # Import the ConfigurationManager.psd1 module 
+    if (-not(Get-Module ConfigurationManager)) {
+        try {
+            Import-Module ("{0}\..\ConfigurationManager.psd1" -f $ENV:SMS_ADMIN_UI_PATH)
+        }
+        catch {
+            $Message = "Failed to import Configuration Manager module"
+            throw $Message
+        }
+    }
+
+    try {
+        # Connect to the site's drive if it is not already present
+        if (-not (Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
+            New-PSDrive -Name $SiteCode -PSProvider CMSite -Root $Server -ErrorAction Stop | Out-Null
+        }
+        # Set the current location to be the site code.
+        Set-Location ("{0}:\" -f $SiteCode) -ErrorAction Stop
+
+        # Verify given sitecode
+        if ((Get-CMSite -SiteCode $SiteCode | Select-Object -ExpandProperty SiteCode) -ne $SiteCode) { throw }
+
+    } 
+    catch {
+        if (-not(Get-PSDrive -Name $SiteCode -PSProvider CMSite -ErrorAction SilentlyContinue)) {
+            Set-Location $Path
+            Remove-PSDrive -Name $SiteCode -Force
+        }
+        $Message = "Failed to create New-PSDrive with site code `"{0}`" and server `"{1}`"" -f $SiteCode, $Server
+        throw $Message
+    }
+
+}
+
+##################################### Edit to specify your site code ################################################
+[string]$mySiteCode = ''
+#####################################################################################################################
+
+Set-CMDrive -SiteCode $mySiteCode
+((Get-CMSoftwareUpdatePointComponent).Props | Where-Object PropertyName -eq "DefaultWSUS").Value2
---
+++ Other/ConfigurationManager/Get-TopSUPname/readme.md
@@ -0,0 +1,9 @@
+# Get-TopSUPName.ps1
+
+## SYNOPSIS
+Running this script will show you what your top-most Software Update Point is. 
+
+
+## DESCRIPTION
+All you need to do is to edit the script on line 50 to specify your site code, then run it on any machine with the ConfigMgr Console installed on it.
+
