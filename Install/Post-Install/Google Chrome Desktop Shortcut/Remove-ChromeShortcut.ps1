5d4f250e4d59b9efbdd24dfe75c64ba275c4b3c3
Scott McAllister <smcallister594@live.co.uk>
2023-03-02 09:22:58 +0000

    Update Remove-ChromeShortcut.ps1
    Changed from -ErrorAction STOP to $ErrorActionPreference = 'Stop'

--- Install/Post-Install/Google Chrome Desktop Shortcut/Remove-ChromeShortcut.ps1
+++ Install/Post-Install/Google Chrome Desktop Shortcut/Remove-ChromeShortcut.ps1
@@ -1,20 +1,21 @@
 #region Remove Desktop shortcut
 ## Remove existing Desktop shortcut
+$ErrorActionPreference = 'Stop'
 Remove-File -Path "$($env:PUBLIC)\desktop\Google Chrome.lnk"
 ## Prevent creation of the Desktop shortcut
 $InstallLocation = Split-Path ((Get-ItemProperty -Path "HKLM:\SOFTWARE\Classes\ChromeHTML\shell\open\command" -Name "(Default)")."(Default)").Split("--")[0].Trim('"',' ') -Parent
 $PrefFile = "$($InstallLocation)\master_preferences"
 if (Test-Path -Path "$PrefFile" -PathType Leaf) {
     try {
-	    $json = Get-Content $PrefFile -ErrorAction Stop | ConvertFrom-Json -ErrorAction Stop
+	    $json = Get-Content $PrefFile | ConvertFrom-Json
 	    If ($json) {
     	    If ($json.distribution -ne $null) {
-        	    $json.distribution | Add-Member -Name "do_not_create_desktop_shortcut" -Value $true -MemberType NoteProperty -Force -ErrorAction Stop
+        	    $json.distribution | Add-Member -Name "do_not_create_desktop_shortcut" -Value $true -MemberType NoteProperty -Force
     	    }
     	    Else {
-        	    $json | Add-Member -Name "distribution" -Value @{"do_not_create_desktop_shortcut"=$true} -MemberType NoteProperty -Force -ErrorAction Stop
+        	    $json | Add-Member -Name "distribution" -Value @{"do_not_create_desktop_shortcut"=$true} -MemberType NoteProperty -Force
     	    }
-    	    $json | ConvertTo-Json -Depth 100  -ErrorAction Stop | Out-File -FilePath $PrefFile -Encoding ASCII -ErrorAction Stop
+    	    $json | ConvertTo-Json -Depth 100 | Out-File -FilePath $PrefFile -Encoding ASCII
 	    }
     }
     catch {
