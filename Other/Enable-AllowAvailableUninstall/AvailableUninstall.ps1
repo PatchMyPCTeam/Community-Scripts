4f84b92d4969a9ea6c2aa6389b23d074df838b5a
Andrew Jimenez <asjimenez91@gmail.com>
2024-08-12 12:17:26 -0700

    Create AvailableUninstall.ps1
    Customer Provided script to enable "Allow Available Uninstall" in Intune for existing apps.

---
+++ Other/Enable-AllowAvailableUninstall/AvailableUninstall.ps1
@@ -0,0 +1,18 @@
+﻿
+
+Connect-MGGraph
+$allapps = Get-MgDeviceAppManagementMobileApp -All # | Out-GridView
+$body = @{
+    "@odata.type"             = "#microsoft.graph.win32LobApp"
+    "allowAvailableUninstall" = $true
+}
+$app = ($allapps[20]).Id
+foreach ($app in $allapps) {
+    $appid = $app.id
+    $graphApiVersion = "beta"
+    $DMS_resource = "deviceAppManagement/mobileApps/$appid"
+    $uri = "https://graph.microsoft.com/$graphApiVersion/$DMS_resource"
+    Write-Host "Enabling Uninstall for: " $app.DisplayName
+    #Update-MgBetaDeviceAppManagementMobileApp -MobileAppId $app.Id -AdditionalProperties ($body)
+    Invoke-MgGraphRequest -Uri $uri -Method Patch -ContentType "application/json" -Body ($body | ConvertTo-Json) -Verbose
+}
